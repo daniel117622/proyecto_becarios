096bb5166fd3cdf42d58b667513bfe78fdcd4deb
Cecoban - Daniel A. Cruz Salazar <DCruz@cecoban.org.mx>
2023-11-21 10:14:23 -0600

    actualizar rutas

---
+++ GamesGPT/Controllers/CarlosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesGPT.Controllers
+{
+    public class CarlosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
